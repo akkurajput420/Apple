f6675dfc7e0e4951ac953d89e6b753d112da3290
akkurajput420 <akashrajput7896543@gmail.com>
2026-01-07 00:13:04 +0530

    Update welcome.py

--- BrandrdXMusic/plugins/tools/welcome.py
+++ BrandrdXMusic/plugins/tools/welcome.py
@@ -91,10 +91,10 @@ async def greet_group(_, member: ChatMemberUpdated):
 ๏ 𝗡𝗔𝗠𝗘 ➠ {user.mention}
 ๏ 𝗜𝗗 ➠ {user.id}
 ๏ 𝐔𝐒𝐄𝐑𝐍𝐀𝐌𝐄 ➠ @{user.username}
-๏ 𝐌𝐀𝐃𝐄 𝐁𝐘 ➠ [𓆩𝗞𝗜𝗡𝗚 📿](https://t.me/NobiCreator)
+๏ 𝐌𝐀𝐃𝐄 𝐁𝐘 ➠ [𓍼 ໋݊ 𝐀 𝐊 𝐀 𝛅 𝐇 ⌯ 🚩!](https://t.me/PremiumMusicUpdate)
 ➖➖➖➖➖➖➖➖➖➖➖
 """,
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"⦿ Join ⦿", url=f"https://t.me/NobiCreator")]])
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(f"⦿ Join ⦿", url=f"https://t.me/PremiumMusicUpdate")]])
         )
     except Exception as e:
         LOGGER.error(e)
